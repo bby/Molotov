0946c26142d2788f78020a5d30c490d4ffbbed06
Tom Burton <tomburton@Raoul-Duke.local>
2011-06-24 10:29:48 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Molotov::Application.config.secret_token = 'e1c2b0b0bb8b0f40de0e97db79c3c31f2c27f1e0c6e2b087f3ff892b694f94ee593acff97a03f17d2e8543683d19e28e35052d827f4b483e89f37109490b7968'
